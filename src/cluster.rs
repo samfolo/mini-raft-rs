a8ea19ee2dbbfaf990388ccbd0eb9cd83ae5b1dc
sfolorunsho <sfolorunsho@thoughtmachine.net>
2025-04-20 13:40:23 +0100

    feat: implement node registration

--- src/cluster.rs
+++ src/cluster.rs
@@ -1,12 +1,19 @@
+use std::sync::{Arc, Mutex};
+
 use tokio::sync::broadcast;
 
 use crate::{cluster_node, server};
 
+type Publisher = Arc<Mutex<broadcast::Sender<server::rpc::ServerRequest>>>;
+type Subscriber = Arc<Mutex<broadcast::Receiver<server::rpc::ServerRequest>>>;
+
+type NodeInit<N> = Box<dyn FnOnce(Publisher, Subscriber) -> N>;
+
 /// A Raft cluster contains several servers
 pub struct Cluster {
-    nodes: Vec<tokio::task::JoinHandle<cluster_node::Result<()>>>,
-    publisher: broadcast::Sender<server::rpc::ServerRequest>,
-    subscriber: broadcast::Receiver<server::rpc::ServerRequest>,
+    nodes: Vec<cluster_node::ClusterNodeHandle>,
+    publisher: Publisher,
+    subscriber: Subscriber,
 }
 
 impl Cluster {
@@ -16,9 +23,19 @@ impl Cluster {
         let (publisher, subscriber) = broadcast::channel(Self::MESSAGE_BUFFER_SIZE);
         Self {
             nodes: Default::default(),
-            publisher,
-            subscriber,
+            publisher: Arc::new(Mutex::new(publisher)),
+            subscriber: Arc::new(Mutex::new(subscriber)),
         }
+    }
+
+    pub async fn register_node<N: cluster_node::ClusterNode>(
+        &mut self,
+        node_init: NodeInit<N>,
+    ) -> &mut Self {
+        let mut node = node_init(Arc::clone(&self.publisher), Arc::clone(&self.subscriber));
+        let handle = tokio::spawn(async move { node.run().await });
+        self.nodes.push(handle);
+        self
     }
 }
 
@@ -26,9 +43,36 @@ impl Cluster {
 mod tests {
     use super::*;
 
-    #[test]
-    fn init() -> anyhow::Result<()> {
-        let _ = Cluster::new();
+    struct MockServer {
+        id: &'static str,
+        tx: Publisher,
+        rx: Subscriber,
+    }
+
+    impl MockServer {
+        fn new(id: &'static str, tx: Publisher, rx: Subscriber) -> Self {
+            Self { id, tx, rx }
+        }
+    }
+
+    impl cluster_node::ClusterNode for MockServer {
+        async fn run(&mut self) -> cluster_node::Result<()> {
+            Ok(())
+        }
+    }
+
+    #[tokio::test]
+    async fn can_register_node() -> anyhow::Result<()> {
+        let mut test_cluster = Cluster::new();
+
+        test_cluster
+            .register_node(Box::new(|tx, rx| {
+                MockServer::new("mock-server-1-id", tx, rx)
+            }))
+            .await;
+
+        assert_eq!(1, test_cluster.nodes.len());
+
         Ok(())
     }
 }
--- src/cluster_node.rs
+++ src/cluster_node.rs
@@ -2,6 +2,8 @@ use tokio::sync::broadcast;
 
 pub type Result<T> = anyhow::Result<T, broadcast::error::RecvError>;
 
-pub trait ClusterNode {
+pub type ClusterNodeHandle = tokio::task::JoinHandle<Result<()>>;
+
+pub trait ClusterNode: Send + 'static {
     fn run(&mut self) -> impl Future<Output = Result<()>> + Send;
 }
