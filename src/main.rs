67c08d8a039c42e91af02e0fe228defbff38e19d
sfolorunsho <sfolorunsho@thoughtmachine.net>
2025-04-25 21:42:31 +0100

    refactor: ensure heartbeat is less than 0.5x min election timeout

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ async fn main() {
     cluster::Cluster::new(1024)
         .with_node_count(5)
         .with_election_timeout_range(750, 1000)
-        .with_heartbeat_interval(100)
+        // Ideally the configured heartbeat interval should be
+        // less than 0.5x the minimum election timeout range:
+        .with_heartbeat_interval(374)
         .run()
         .await;
 }
